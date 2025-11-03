ebbdbface8f0f07da5ebdd1a283176d4808f69ab
Torab-spec <torabpwskill@gmail.com>
2025-11-03 22:14:25 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# <ins> JAVA BY PRASANT SIR </ins>
+## Date : 01/11/2025
+
+# Part - 1
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello java");
+
+        System.out.println(" " +
+                "echo \"# JavaProgram_02\" >> README.md\n" +
+                "git init\n" +
+                "git add README.md\n" +
+                "git commit -m \"first commit\"\n" +
+                "git branch -M main\n" +
+                "git remote add origin https://github.com/Torab-spec/JavaProgram_02.git\n" +
+                "git push -u origin main");
+
+
+
+    }
+}
