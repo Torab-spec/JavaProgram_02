e7b8dd75f7dd03f56d9cc7d304b821e4716cc7ac
Torab-spec <torabpwskill@gmail.com>
2025-11-05 21:09:56 +0530

    051125

---
+++ src/Challenge02.java
@@ -0,0 +1,15 @@
+public class Challenge02 {
+    public static void main(String[] args) {
+        System.out.println("First Answer");
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n");
+
+        System.out.println("Second Answer");
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+
+        System.out.println("Third Answer");
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *\n");
+
+
+
+    }
+}
