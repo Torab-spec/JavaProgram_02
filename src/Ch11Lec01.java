82f477ea8adcaf80a0b26847553ad63cea256fd2
Torab-spec <torabpwskill@gmail.com>
2026-01-08 00:16:24 +0530

    070126 ch11Lec01

--- src/Ch11Lec01.java
+++ src/Ch11Lec01.java
@@ -1,6 +1,9 @@
+
 public class Ch11Lec01 {
+
     //11.1 -> Variable Arguments
     public static void main(String[] args) {
+        // see - chapterEleven.lecture.Ch11Lecs01
 
     }
 }
--- src/chapterEleven/lecture01/Ch11Lecs01.java
+++ src/chapterEleven/lecture01/Ch11Lecs01.java
@@ -2,6 +2,30 @@ package chapterEleven.lecture01;
 
 public class Ch11Lecs01 {
     public static void main(String[] args) {
+        System.out.println("Passing Zero arguments : " + sum());
+        System.out.println("Passing one arguments : " + sum(2));
+        System.out.println("Passing two arguments : " + sum(2, 3));
+        System.out.println("Passing three arguments : " + sum(2, 3, 4));
+        System.out.println(" ");
 
+        System.out.println("Passing two arguments : " + add(2, 3));
+        System.out.println("Passing three arguments : " + add(2, 3, 4));
+
+    }
+
+    public static int sum(int... a) {
+        int sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int add(int x, int y, int... a) {
+        int add = 0;
+        for (int i : a) {
+            add += i;
+        }
+        return add;
     }
 }
