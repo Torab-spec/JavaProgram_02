47e92c8320d0b196f20a4a02c558c7d7381e10c1
Torab-spec <torabpwskill@gmail.com>
2026-01-08 21:35:54 +0530

    080126 ch11Lec02

---
+++ src/Ch11Lec02.java
@@ -0,0 +1,9 @@
+public class Ch11Lec02 {
+    //11.2 -> Wrapper Classes
+    public static void main(String[] args) {
+//        Integer num1 = Integer.valueOf(6);
+        Integer num1 = 6;
+//        int num2 = null;
+        Integer num3 = null;
+    }
+}
