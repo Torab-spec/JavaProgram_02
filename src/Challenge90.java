7dd9c912757e5376d21f426952338bd795a313d0
Torab-spec <torabpwskill@gmail.com>
2026-01-10 15:26:17 +0530

    100126 ch11Lec90

--- src/Challenge90.java
+++ src/Challenge90.java
@@ -1,6 +1,18 @@
 public class Challenge90 {
     //
     public static void main(String[] args) {
-
+        System.out.println(concatenate("Md"));
+        System.out.println(concatenate("Abu"));
+        System.out.println(concatenate("Torab"));
+        System.out.println(concatenate("Uddin"));
     }
+
+    public static String concatenate(String... strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str).append(" ");
+        }
+        return sb.toString();
+    }
+
 }
